a30ac5ba304d5ed9c95f62f0dea34eb0a0e9da66
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-05-14 13:40:50 +0530

    Add operation

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static int addition(int num1,int num2){
+        return num1+num2;
+    }
+    public static void main(String[] args){
+        int num = addition(1,2);
+        System.out.println(" Welcome to my calculator Program...");
+        System.out.println("Addition is: "+num);
+    }    
+}
